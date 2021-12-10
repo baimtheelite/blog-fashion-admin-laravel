c8ca9ced219f12af88ad9546e531c08f393f058b
Ibrahim Ahmad Jabar <30390302+baimtheelite@users.noreply.github.com>
2021-12-11 01:02:57 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'api/article/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
